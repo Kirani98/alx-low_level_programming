2aa12aac055a3baa364da84cb8eaf8cb2ca9c7a9
Kirani98 <naneekee2@gmail.com>
2022-07-13 23:33:42 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Function entry point
+ * @c: Function param
+ * Desc:  a function that checks for uppercase character
+ *
+ * Return: 1 if uppercase letter is found else 0
+ */
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	return (1);
+
+	return (0);
+}
