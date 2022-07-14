5042365ea0c45b03764df95e12373de622fc6c3d
Kirani98 <naneekee2@gmail.com>
2022-07-13 17:23:17 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int debug;
+
+	debug = 0;
+	positive_or_negative(debug);
+
+	return (0);
+}
